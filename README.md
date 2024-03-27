98cb8fd53f784f52a6643b4ceb60f68c88b4805e
kolserdav <serega12101983@gmail.com>
2024-03-27 14:16:47 +0700

    create readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Create TIFF
+Create tiff image from a list of images
+
+## Requirements
+- Python: ^3.11.8
+- Pip: ^24.0
+
+## Installation
+1. Install dependencies:
+```sh
+pip install -r requirements.txt 
+```
+2. Copy file `.env.example` to `.env`
+
+## Configuration
+Edit file `.env`:
+```ini
+# Source images path
+IMAGES_PATH=/abs/path/to/images/dir
+
+# Result image dimensions
+WIDTH=1920
+HEIGHT=1080
+
+# List of excluded dirs or files
+#EXCLUDE_NAMES=1388_12_Наклейки 3-D_3,1388_6_Наклейки 3-D_2
+```
+## Usage
+Run:
+```sh
+python main.py
+```
+Show results in `tmp/result.tiff`
--- main.py
+++ main.py
@@ -1,34 +1,93 @@
-from libtiff import TIFF
-import numpy as np
 from PIL import Image
 from dotenv import load_dotenv
 from os import listdir, getenv
-from os.path import isfile, join,abspath
+from os.path import isfile, join, abspath
 from typing import List
 
 load_dotenv()
 
+# Constants
+RESULT_FILE_NAME='result.tif'
+ERROR = '[ERROR]'
+INFO = '[INFO]'
+WARN = '[WARN]'
 
+## Dependends of .gitignore
+RESULT_PATH = join(abspath('./tmp'), RESULT_FILE_NAME)
+EXCLUDE_FILES = ['.gitkeep', RESULT_FILE_NAME]
+
+# Env variables
+EXCLUDE_NAMES = (getenv('EXCLUDE_NAMES') or '').split(',')
 IMAGES_PATH = getenv('IMAGES_PATH')
+EXCLUDE_FILES= EXCLUDE_FILES + EXCLUDE_NAMES
+WIDTH = int(getenv('WIDTH')) or 1920
+HEIGHT = int(getenv('HEIGHT')) or 1080
 
 def read_images_dir(path: str):
   files: List[str] = []
-  for item in listdir(path):
+  dir = []
+  try:
+   dir = listdir(path)
+  except:
+    print('[ERROR]', 'Failed to read images dir, check env variable IMAGES_PATH:' + IMAGES_PATH)
+  for item in dir:
     full_path = join(path, item)
+    if item in EXCLUDE_FILES:
+      if item in EXCLUDE_NAMES:
+        print(WARN, 'Excluded item:', full_path)
+      continue
     if isfile(full_path):
       files.append(full_path)
     else:
       for _item in read_images_dir(full_path):
         files.append(_item)
+  print(INFO, 'Read directory:', path)
   return files
 
+def get_max_sizes(length: int, shift: int):
+  s_all = WIDTH * HEIGHT
+  s_one = s_all / length
+  width = int(s_one ** (0.5)) - shift
+  height = width
+  row_count = int(WIDTH / width)
+  col_count = int(HEIGHT / height)
+  if (row_count * col_count < length):
+    return get_max_sizes(length, shift + 10)
+  shift_x = int((WIDTH - width * row_count) / row_count)
+  shift_y = int((HEIGHT - height * col_count) / col_count)
+  res = (width, height, row_count, shift_x, shift_y)
+  print(INFO, 'Items sizes:', res)
+  return res
+
+
 def create_tiff_from_images(src_path: str, dest_path: str):
-  read_images_dir(src_path)
+  src_images = read_images_dir(src_path)
+  (width, height, row_count, shift_x, shift_y) = get_max_sizes(len(src_images), 0)
+  result = Image.new(size=(WIDTH, HEIGHT), mode='RGB')
 
-create_tiff_from_images(src_path=IMAGES_PATH, dest_path=abspath('./tmp/result.tif'))
+  col = 0
+  row = 1
+  x = int(shift_x / 2)
+  y = int(shift_y / 2)
+  for item in src_images:
+    if col < row_count:
+      x = col * (width + shift_x) or int(shift_x / 2)
+      col += 1
+    else:
+      x = int(shift_x / 2)
+      col = 1
 
-data = np.random.randint(0, 255, (10,10)).astype(np.uint8)
-im = Image.fromarray(data)
-im.save('test.tif')
+      y = row * (height + shift_y)
+      row += 1
+    
+    img = Image.open(item)
+    img = img.resize(size=(width, height))
+    result.paste(img, (x,  y ))
+  
+  result.save(dest_path, format='TIFF')
+  print(INFO, 'Result saved:', dest_path)
 
-tif = TIFF.open('test.tif', mode='r')
+create_tiff_from_images(src_path=IMAGES_PATH, dest_path=RESULT_PATH)
+
+
+
